56f7dfce62a79ec3822eceb17d450706ef1996ff
Jay Park <flexiblethink@gmail.com>
2017-05-31 12:35:09 +0900

    MyVideoView를 통한 전체화면 영상재생문제를 처리중

--- app/src/main/java/a83level/coffee_map/MainActivity.java
+++ app/src/main/java/a83level/coffee_map/MainActivity.java
@@ -1,7 +1,7 @@
 package a83level.coffee_map;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/a83level/coffee_map/SplashScreenActivity.java
+++ app/src/main/java/a83level/coffee_map/SplashScreenActivity.java
@@ -1,64 +1,37 @@
 package a83level.coffee_map;
 
-/**
- * Created by flexi on 5/30/2017.
- */
-
-import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.VideoView;
-import android.view.MotionEvent;
 
-/* public class SplashActivity extends Activity {
+/**
+ * Created by flexi on 5/30/2017.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        VideoView vv = (VideoView)findViewById(R.id.videoView);
 
-        try{
-            Thread.sleep(4000); // waiting for 4 secs
-        } catch(InterruptedException e) {
-            e.printStackTrace();
-        }
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash2);
+        vv.setVideoURI(video);
+
+        vv.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                startNextActivity();
+            }
+        });
+
+        vv.start();
     }
 
-}
-*/
-
-public class SplashScreenActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        try {
-            VideoView videoHolder = new VideoView(this);
-            setContentView(videoHolder);
-            Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.splash);
-            videoHolder.setVideoURI(video);
-
-            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                public void onCompletion(MediaPlayer mp) {
-                    jump();
-                }
-            });
-            videoHolder.start();
-        } catch (Exception ex) {
-            jump();
-        }
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        jump();
-        return true;
-    }
-
-    private void jump() {
+    private void startNextActivity() {
         if (isFinishing())
             return;
         startActivity(new Intent(this, MainActivity.class));
